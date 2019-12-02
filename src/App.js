f4cb3ca2a4e88a77ca2f615a71b67c4d78e7c413
james lawson <jlawson079@gmail.com>
2019-12-02 14:40:31 -0800

    completed seciton 2 -React Portfolio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
-
-// class RegularClass {}
-// class ComponentClass extends Component{}
-
-// const regularClassInstance = new RegularClass();
-// const componentClassInstance = new ComponentClass();
-
-// console.log('regularClassInstance', regularClassInstance);
-// console.log('componentClassInstance', componentClassInstance);
-
-
+import Projects from './Projects';
+import SocialProfiles from './SocialProfiles'; 
+import profile from './assets/profile.png'
 class App extends Component {
-render(){
+    state = { displayBio: false };
+    
+
+
+toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio });
+    // this.state.displayBio = !this.state.displayBio; do not modify state directly
+}
+
+render () {
     return (
         <div>
+            <img src={profile} alt = 'profile' className='profile' />
             <h1>Hello!</h1>
             <p>My name is James. I'm a Software Engineer</p>
             <p>I'm always looking for more meaningful projects</p>
+            {this.state.displayBio ?(
+        <div>
+            <p>I live in San Carlos, I code everyday.</p>
+            <p>My favorite language is JS, I think React JS is an excellent framework to have in one's toolbelt.</p>
+            <p>Besides coding, I like photography and skateboarding.</p>
+            <button onClick={this.toggleDisplayBio}>Show Less</button>
+        </div>
+    ) : (
+        <div>
+            <button onClick={this.toggleDisplayBio }>Read more</button>
+        </div>
+    )
+    }
+    <hr />
+    <Projects />
+    <hr />
+    <SocialProfiles />
         </div>
     )
 }
